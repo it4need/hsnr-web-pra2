9b0c9d8c90d56f0a7a00f9befb2a495210ef82f8
Jan André Schlösser <jan.schloesser@it4need.de>
2018-12-26 14:08:14 +0100

    :construction: Work in progress - bug controller

--- app/controllers/BugController.py
+++ app/controllers/BugController.py
@@ -14,7 +14,7 @@ import cherrypy
 
 class BugController(RESTController):
     def __init__(self):
-        required_attributes = ['name', 'description', 'qs_employee_id', 'component_id', 'cause_id']
+        required_attributes = ['name', 'description', 'qs_employee_id', 'component_id', 'categories']
         RESTController.__init__(self, required_attributes)
 
     def _setupRESTfulModels(self):
@@ -36,12 +36,18 @@ class BugController(RESTController):
             if self.__isValidBugTypeForUpdate():
                 raise ValidationException("An existing bug can only update their type to 1 or 2.")
 
+            if 'type' in cherrypy.request.json and cherrypy.request.json['type'] == Bug.TYPE_RESOLVED and \
+                    'cause_id' not in cherrypy.request.json:
+                raise ValidationException("An resolved bug must have a valid `cause_id´.")
+
             if Bug().find(int(id))[0]['type'] is Bug.TYPE_RESOLVED or ('type' in cherrypy.request.json and \
-                    cherrypy.request.json['type'] == Bug.TYPE_RESOLVED):
-                self.__updateSolvedCategories(id)
+                                                                       cherrypy.request.json[
+                                                                           'type'] == Bug.TYPE_RESOLVED):
 
                 if Bug().find(int(id))[0]['solved_date'] is None:
                     additional_params = {'solved_date': str(strftime("%Y-%m-%d %H:%M:%S", localtime()))}
+
+            self.__updateSolvedCategories(id)
 
             return super(BugController, self).update(id, additional_params, ['created_date'])
 
@@ -60,35 +66,45 @@ class BugController(RESTController):
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def store(self):
-        non_allowable_attr = ['sw_employee_id', 'type', 'solution_description', 'solved_date', 'solved_categories',
+        non_allowable_attr = ['sw_employee_id', 'type', 'solution_description', 'solved_date', 'cause_id',
                               'created_date']
 
         try:
             self._checkRequestForExistingModelEntry(
                 [ProjectComponents(), 'component_id'],
-                [Employee(), 'qs_employee_id'],
-                [Cause(), 'cause_id']
+                [Employee(), 'qs_employee_id']
             )
 
-            return super(BugController, self).store(
+            if 'categories' in cherrypy.request.json and len(cherrypy.request.json['categories']) < 1:
+                raise ValidationException('A newly created bug must have a minimum of one `categories´.')
+
+            result = super(BugController, self).store(
                 {'type': Bug.TYPE_REPORTED, 'created_date': str(strftime("%Y-%m-%d %H:%M:%S", localtime()))},
                 non_allowable_attr
             )
+
+            if result['code'] == 201:
+                self.__updateSolvedCategories(result['data']['id'])
+
+                return self.withSuccessfullyCreated(Bug().find(result['data']['id']))
+            else:
+                return self.withError(result)
+
         except ValidationException as e:
             return self.withError(str(e))
 
     def __updateSolvedCategories(self, id):
-        if 'solved_categories' in cherrypy.request.json:
-            if not isinstance(cherrypy.request.json['solved_categories'], list):
-                raise ValidationException('`solved_categories´ must be an instance of list.')
+        if 'categories' in cherrypy.request.json:
+            if not isinstance(cherrypy.request.json['categories'], list):
+                raise ValidationException('`categories´ must be an instance of list.')
 
-            number_of_valid_categories = len(cherrypy.request.json['solved_categories'])
+            number_of_valid_categories = len(cherrypy.request.json['categories'])
 
-            for category in cherrypy.request.json['solved_categories']:
+            for category in cherrypy.request.json['categories']:
                 if Category().find(category):
                     number_of_valid_categories -= 1
                 else:
-                    raise ValidationException("A category with the given `solved_categories´ list (ID: " + str(
+                    raise ValidationException("A category with the given `categories´ list (ID: " + str(
                         category) + ") does not exist.")
 
             if number_of_valid_categories == 0:
@@ -97,7 +113,7 @@ class BugController(RESTController):
                 for existing_bug_categories in all_bug_categories:
                     BugCategories().delete(existing_bug_categories['id'])
 
-                for category in cherrypy.request.json['solved_categories']:
+                for category in cherrypy.request.json['categories']:
                     BugCategories().create({'bug_id': int(id), 'category_id': int(category)})
 
         return True
@@ -105,4 +121,4 @@ class BugController(RESTController):
     def __isValidBugTypeForUpdate(self):
         return 'type' in cherrypy.request.json and \
                (int(cherrypy.request.json['type']) > Bug.TYPE_SOULTION_VERIFIED or int(cherrypy.request.json[
-                   'type']) < Bug.TYPE_RESOLVED)
+                                                                                           'type']) < Bug.TYPE_RESOLVED)
